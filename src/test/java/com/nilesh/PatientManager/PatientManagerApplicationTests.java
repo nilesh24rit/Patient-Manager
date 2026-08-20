e756b796787d38fb6a7a58cb18d9fe74e6d3ebe0
nilesh24rit <nilesh.rit.24@gmail.com>
2026-08-20 16:51:19 +0530

    Spring Initializr

---
+++ src/test/java/com/nilesh/PatientManager/PatientManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nilesh.PatientManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
