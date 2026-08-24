06720a4d9eba5f8a8ccf2826ec13c21ca57aa7ee
nilesh24rit <nilesh.rit.24@gmail.com>
2026-08-24 22:34:48 +0530

    Patient Request DTO

---
+++ src/main/java/com/nilesh/PatientManager/dto/PatientRequestDTO.java
@@ -0,0 +1,26 @@
+package com.nilesh.PatientManager.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class PatientRequestDTO {
+
+    @NotBlank(message = "Name cannot be blank")
+    @Size(max = 100,message = "Name cannot exceed 100 characters")
+    private String name;
+
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Enter a valid email")
+    private String email;
+
+    @NotBlank(message = "Address cannot be blank")
+    private String address;
+
+    @NotBlank(message = "Required Date of Birth")
+    private String dob;
+
+    @NotBlank(message = "Required regestration date")
+    private String regestrationDate;
+
+}
