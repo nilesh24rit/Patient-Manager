411a3f14fa292b63d6c44e5d20de421bc6fcc628
nilesh24rit <nilesh.rit.24@gmail.com>
2026-08-28 21:12:28 +0530

    Created Detailed API documentation + fix SonarQube code smells in PatientController

--- src/main/java/com/nilesh/PatientManager/controller/PatientController.java
+++ src/main/java/com/nilesh/PatientManager/controller/PatientController.java
@@ -14,7 +14,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 import java.util.List;
 import java.util.UUID;
@@ -23,6 +22,7 @@ import java.util.UUID;
 @RequestMapping("/patient")
 @Tag(name = "Patient Controller", description = "Endpoints for managing patients")
 public class PatientController {
+
     private final PatientService patientService;
 
     public PatientController(PatientService patientService) {
@@ -34,20 +34,22 @@ public class PatientController {
             summary = "Get All Patients",
             responses = {@ApiResponse(responseCode = "200", description = "Patients retrieved"),
                     @ApiResponse(responseCode = "404", description = "No patients found")})
-    public ResponseEntity<List<PatientResponseDto>> getall(){
+    public ResponseEntity<List<PatientResponseDto>> getAllPatients() {
         List<PatientResponseDto> allPatient = patientService.getAllPatient();
-        if(allPatient!=null) return ResponseEntity.ok().body(allPatient);
+        if (allPatient != null) {
+            return ResponseEntity.ok().body(allPatient);
+        }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @PostMapping("/create-patient")
-    @Operation(summary = "Create Patient", description = "Create a new patient",requestBody =
+    @Operation(summary = "Create Patient", description = "Create a new patient", requestBody =
     @io.swagger.v3.oas.annotations.parameters.RequestBody(
-            description = "Patient details to create", required = true,content = @Content(
+            description = "Patient details to create", required = true, content = @Content(
             mediaType = "application/json", schema = @Schema(implementation = PatientRequestDTO.class))),
             responses = {@ApiResponse(responseCode = "200", description = "Patient created successfully"),
                     @ApiResponse(responseCode = "400", description = "Invalid input data")})
-    public ResponseEntity<PatientResponseDto> createPatient(@Valid @RequestBody PatientRequestDTO patientRequestDTO){
+    public ResponseEntity<PatientResponseDto> createPatient(@Valid @RequestBody PatientRequestDTO patientRequestDTO) {
         PatientResponseDto patientResponseDto = patientService.createPatient(patientRequestDTO);
         return ResponseEntity.ok().body(patientResponseDto);
     }
@@ -56,25 +58,21 @@ public class PatientController {
     @Operation(summary = "Update Patient", description = "Update an existing patient",
             requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
                     description = "Updated patient details", required = true, content = @Content(
-                            mediaType = "application/json", schema = @Schema(implementation = PatientRequestDTO.class))),
+                    mediaType = "application/json", schema = @Schema(implementation = PatientRequestDTO.class))),
             responses = {@ApiResponse(responseCode = "200", description = "Patient updated successfully"),
                     @ApiResponse(responseCode = "400", description = "Invalid input data"),
                     @ApiResponse(responseCode = "404", description = "Patient not found")})
     public ResponseEntity<PatientResponseDto> updatePatient(
-            @PathVariable UUID id, @Validated({Default.class}) @RequestBody PatientRequestDTO patientRequestDTO){
+            @PathVariable UUID id, @Validated({Default.class}) @RequestBody PatientRequestDTO patientRequestDTO) {
         PatientResponseDto patientResponseDto = patientService.updatePatient(id, patientRequestDTO);
         return ResponseEntity.ok().body(patientResponseDto);
     }
 
     @DeleteMapping("/delete-patient/{id}")
     @Operation(summary = "Delete Patient", description = "Delete an existing patient",
-            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
-            description = "Patient ID to delete", required = true, content = @Content(
-                    mediaType = "application/json", schema = @Schema(implementation = UUID.class))),
             responses = {@ApiResponse(responseCode = "200", description = "Patient deleted successfully"),
                     @ApiResponse(responseCode = "404", description = "Patient not found")})
     public ResponseEntity<PatientResponseDto> deletePatient(@PathVariable UUID id) {
-        ResponseEntity<PatientResponseDto> response = patientService.deletePatient(id);
-        return response;
+        return patientService.deletePatient(id);
     }
 }
