be367476d8c41864b9e14b72119a5da256f761d4
nilesh24rit <nilesh.rit.24@gmail.com>
2026-09-03 20:34:13 +0530

    add @Service to create a bean

--- patient-service/src/main/java/com/nilesh/PatientManager/kafka/kafkaProducer.java
+++ patient-service/src/main/java/com/nilesh/PatientManager/kafka/kafkaProducer.java
@@ -3,8 +3,10 @@ package com.nilesh.PatientManager.kafka;
 import com.nilesh.PatientManager.model.Patient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
 import patient.event.PatientEvent;
 
+@Service
 @Slf4j
 public class kafkaProducer {
     private final KafkaTemplate<String, byte[]> kafkaTemplate;
