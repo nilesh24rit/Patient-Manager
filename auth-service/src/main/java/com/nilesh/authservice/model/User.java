b43fa157086536a86e4020428f925d6cc1035fe9
nilesh24rit <nilesh.rit.24@gmail.com>
2026-09-09 20:48:16 +0530

    User model

---
+++ auth-service/src/main/java/com/nilesh/authservice/model/User.java
@@ -0,0 +1,28 @@
+package com.nilesh.authservice.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Email
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+}
