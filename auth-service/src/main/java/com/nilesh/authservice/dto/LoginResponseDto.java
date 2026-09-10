6b7e1dd91242dffcaa37f41cbc7d0c8814eb670c
nilesh24rit <nilesh.rit.24@gmail.com>
2026-09-10 21:12:42 +0530

    Constructor for JWT token

---
+++ auth-service/src/main/java/com/nilesh/authservice/dto/LoginResponseDto.java
@@ -0,0 +1,9 @@
+package com.nilesh.authservice.dto;
+
+public class LoginResponseDto {
+    private String token;
+
+    public LoginResponseDto(String token) {
+        this.token = token;
+    }
+}
