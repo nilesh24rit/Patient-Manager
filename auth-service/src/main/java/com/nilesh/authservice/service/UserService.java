7858bd38e4ab311cd010bd94a58ef6a6a72b1f43
nilesh24rit <nilesh.rit.24@gmail.com>
2026-09-11 21:13:13 +0530

    add user service email lookup

---
+++ auth-service/src/main/java/com/nilesh/authservice/service/UserService.java
@@ -0,0 +1,18 @@
+package com.nilesh.authservice.service;
+
+import com.nilesh.authservice.Repository.UserRepository;
+import com.nilesh.authservice.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+}
