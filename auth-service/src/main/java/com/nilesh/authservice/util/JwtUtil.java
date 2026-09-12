4313141bfdb15e4a0749ffda32b145e647b9555f
nilesh24rit <nilesh.rit.24@gmail.com>
2026-09-12 20:50:17 +0530

    Validate token

--- auth-service/src/main/java/com/nilesh/authservice/util/JwtUtil.java
+++ auth-service/src/main/java/com/nilesh/authservice/util/JwtUtil.java
@@ -1,10 +1,15 @@
 package com.nilesh.authservice.util;
 
-import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SecurityException;
+import io.jsonwebtoken.security.SignatureException;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Base64;
@@ -26,5 +31,20 @@ public class JwtUtil {
                 .expiration(new Date(System.currentTimeMillis() +1000 * 60 * 60)) // 1 hour expiration
                 .signWith(key)
                 .compact();
+    }
+
+    public void validateToken(String token){
+
+        try {
+            Jwts.parser().verifyWith((SecretKey) key)
+                    .build()
+                    .parseClaimsJws(token);
+        }
+        catch (SignatureException e) {
+            throw new JwtException("Invalid JWT signature", e);
+        }
+        catch (JwtException e) {
+            throw new JwtException("Invalid JWT token", e);
+        }
     }
 }
